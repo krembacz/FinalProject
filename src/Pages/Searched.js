2107a79cfa411611966f8586b21653faf6fc8fe9
krembacz <114125166+krembacz@users.noreply.github.com>
2023-02-23 22:47:01 -0700

    Update Searched.js

--- src/Pages/Searched.js
+++ src/Pages/Searched.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom';
 import { Row } from 'react-bootstrap';
+import '../StyleSheets/HomePageStyle.css';
 import RecipeCard from '../Components/RecipeCard';
 import HeroBannerRecipe from '../Components/HeroBannerRecipe'
-import '../StyleSheets/HomePageStyle.css';
 
 export default function Searched() {
+    //separate page that displays when the search button is clicked to show results
     const [searchedRecipes, setSearchedRecipes] = useState([]);
     let params = useParams();
 
+    //utilizes input from searchbar to filter API results and return them 
     const getSearched = async (name) => {
         const response = await fetch(`https://63e6e5dd15d793f46f87f0f3.mockapi.io/recipes?recipename=${name}`);
         const data = await response.json();
