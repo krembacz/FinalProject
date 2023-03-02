8ce12c0c2286277d9341f6366419600058bd2a52
krembacz <114125166+krembacz@users.noreply.github.com>
2023-03-02 16:14:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,241 +1,101 @@
-import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import { Form, Row } from 'react-bootstrap';
-import { FaEdit, FaTrash } from 'react-icons/fa';
-import HeroBannerIndividual from '../Components/HeroBannerIndividual';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import '../StyleSheets/RecipePage.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import RecipesPage from './Pages/RecipesPage';
+import AddRecipePage from './Pages/AddRecipePage';
+import About from './Pages/About';
+import NavigationBar from './Components/NavigationBar';
+import RecipePage from './Pages/RecipePage';
+import { useState, useEffect } from 'react';
+import Searched from './Pages/Searched';
 
-export default function RecipePage({ recipeList, deleteRecipe, submitEditedRecipe, setId }) {
-    //using recipeId as a paraameter to assign correct recipe info to cards
-    let { recipeId } = useParams();
-    const recipe = recipeList.find(r => r.id === recipeId);
+export default function App() {
+  const API = "https://63e6e5dd15d793f46f87f0f3.mockapi.io/recipes";
+  const [recipeList, setRecipeList] = useState([]);
+  const [id, setId] = useState("");
+  const [featuredRecipe, setFeaturedRecipe] = useState({});
+  const [isReady, setIsReady] = useState(null);
 
-    //modal show and handleshow 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    //state for the edited recipe - this is an empty object that take input 
-    const [editedRecipe, setEditedRecipe] = useState({
-        recipename: "",
-        contributor: "",
-        preptime: "",
-        totaltime: "",
-        ingredients: "",
-        summary: "",
-        steps: "",
-        image: ""
-    })
-
-    //takes two parameters, inputToEdit and event. Used spread operator for myobject 
-    //spread out an existing recipe. Edited recipe is an object with the key value pairs that exist in recipe
-    //necessary to have function be unique for each input 
-    const handleChange = (inputToEdit, e) => {
-        let myObject = { ...editedRecipe }
-        myObject[inputToEdit] = e.target.value
-        setEditedRecipe(myObject);
-        console.log(editedRecipe);
-    };
-
-    //takes input from edit forms and uses function in App to PUT changes
-    const handleEditedSubmit = (myEditedRecipe) => {
-        submitEditedRecipe(myEditedRecipe);
-        console.log(recipeList);
-        navigateRecipes();
+  useEffect(() => {
+    const refreshInfo = async () => {
+      const response = await fetch(API);
+      const data = await response.json();
+      setRecipeList(data);
+      setIsReady(data);
+      console.log(data);
     }
+    refreshInfo();
+  }, []);
 
-    //navigates to recipes page on click 
-    let navigate = useNavigate();
-    const navigateRecipes = () => {
-        navigate("/recipes");
-    };
+  useEffect(() => {
+    if (isReady === null) {
+      return;
+    }
+    getFeaturedRecipe();
+  }, [isReady]);
 
+  const getFeaturedRecipe = async () => {
+    const index = [Math.floor(Math.random() * recipeList.length)]
+    const response = await fetch(API + "/" + index)
+    const data = await response.json();
+    setFeaturedRecipe(data);
+  }
 
-    return (
-        <div>
-            <HeroBannerIndividual />
-            <br /><br />
-            <div className="recipe-page-container">
-                <div className="header-info">
-                    <h1> {recipe.recipename} </h1>
+  const getRecipes = async () => {
+    const response = await fetch(API);
+    const data = await response.json();
+    setRecipeList(data);
+    console.log(data);
+  };
 
-                    <div className="contributor flex"> Recipe by: {recipe.contributor}
-                        <div className="icons">
-                            <FaEdit className="icon" size={20} onClick={() => {
-                                handleShow();
-                                setId(recipeId);
-                                setEditedRecipe(recipe);
-                            }} />
+  const addRecipeClick = async (newRecipe) => {
+    newRecipe.ingredients = newRecipe.ingredients.split(",").map(items => items.trim())
+    newRecipe.steps = newRecipe.steps.split(",").map(items => items.trim())
 
-                            <FaTrash className="icon" size={18} onClick={() => {
-                                deleteRecipe(recipe.id);
-                                navigateRecipes();
-                            }} />
-                        </div>
-                    </div>
-                </div>
-
-                <Row>
-                    <div className="img-container col-sm-12 col-md-11 col-lg-7">
-                        <img src={recipe.image} alt="image of given recipe" className="recipe-image" />
-                    </div>
-
-                    <div className=" stack summary col-sm-12 col-md-12 col-lg-5"> {recipe.summary}
-                        <h3>Ingredients</h3>
-                        <ul className="ingredients">
-                            {recipe.ingredients.map((ingredient, index) => <li key={index}> {ingredient}</li>)}
-                        </ul>
-                    </div>
-                </Row>
-
-                <Row>
-                    <div className="steps-container">
-                        <h3>Steps</h3>
-                        <ul >
-                            {recipe.steps.map((step, index) => <li className="steps" key={index}>{step}</li>)}
-                        </ul>
-                    </div>
-                </Row>
-
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Edit recipe </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <div className="class-inline mb-5">
-                                <Form.Group className="col-6">
-                                    <Form.Label>  Recipe + Restriction </Form.Label>
-                                    <Form.Control
-                                        name="recipename"
-                                        type="text"
-                                        placeholder={recipe.recipename}
-                                        required
-                                        defaultValue={recipe.recipename}
-                                        onChange={(e) => handleChange("recipename", e)}
-                                    />
-                                </Form.Group>
-
-                                <Form.Group className="col-5">
-                                    <Form.Label> Contributor </Form.Label>
-                                    <Form.Control
-                                        name="contributor"
-                                        type="text"
-                                        placeholder={recipe.contributor}
-                                        required
-                                        defaultValue={recipe.contributor}
-                                        onChange={(e) => handleChange("contributor", e)}
-                                    />
-                                </Form.Group>
-                            </div>
-
-                            <Form.Group className="mb-5">
-                                <Form.Label> Recipe Image - horizontal images only! </Form.Label>
-                                <Form.Control
-                                    name="image"
-                                    type="text"
-                                    placeholder="Paste in an image URL for your recipe"
-                                    required
-                                    defaultValue={recipe.image}
-                                    onChange={(e) => handleChange("image", e)}
-                                />
-                            </Form.Group>
-
-                            <Form.Group className="mb-5">
-                                <Form.Label> Recipe Summary </Form.Label>
-                                <Form.Control
-                                    name="summary"
-                                    as="textarea"
-                                    cols={5}
-                                    rows={2}
-                                    placeholder={recipe.summary}
-                                    required
-                                    defaultValue={recipe.summary}
-                                    onChange={(e) => handleChange("summary", e)}
-                                />
-                            </Form.Group>
-
-                            <div className="class-inline-short">
-                                <Form.Group className="col-4">
-                                    <Form.Label>
-                                        Prep &#x2022; minutes
-                                    </Form.Label>
-                                    <Form.Control
-                                        name="preptime"
-                                        type="number"
-                                        placeholder={recipe.preptime}
-                                        required
-                                        defaultValue={recipe.preptime}
-                                        onChange={(e) => handleChange("preptime", e)}
-                                    />
-                                </Form.Group>
-
-                                <Form.Group className="col-5 mb-3">
-                                    <Form.Label>
-                                        Total Time &#x2022; minutes
-                                    </Form.Label>
-                                    <Form.Control
-                                        name="totaltime"
-                                        type="number"
-                                        placeholder={recipe.totaltime}
-                                        required
-                                        defaultValue={recipe.totaltime}
-                                        onChange={(e) => handleChange("totaltime", e)}
-                                    />
-                                </Form.Group>
-                            </div>
-
-                            <Form.Group className="mb-3">
-                                <Form.Label> Ingredients &#x2022; Separate items by commas </Form.Label>
-                                <Form.Control
-                                    name="ingredients"
-                                    as="textarea"
-                                    cols={10}
-                                    rows={2}
-                                    placeholder={recipe.ingredients}
-                                    required
-                                    onChange={(e) => handleChange("ingredients", e)}
-                                    defaultValue={recipe.ingredients}
-                                />
-                            </Form.Group>
-                            <br /><br />
+    await setRecipeList(recipeList.concat(newRecipe))
+    await fetch(API, {
+      method: "POST",
+      body: JSON.stringify(newRecipe),
+      headers: { "Content-Type": "application/json" },
+    })
+    console.log(recipeList);
+    getRecipes();
+  }
 
 
-                            <Form.Group className="mb-3" controlid="recipeInput">
-                                <Form.Label> Recipe Steps &#x2022; Separate items by commas </Form.Label>
-                                <Form.Control
-                                    name="steps"
-                                    as="textarea"
-                                    cols={10}
-                                    rows={4}
-                                    placeholder={recipe.steps}
-                                    required
-                                    defaultValue={recipe.steps}
-                                    onChange={(e) => handleChange("steps", e)}
-                                />
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
+  const submitEditedRecipe = async (editedRecipe) => {
+    console.log(editedRecipe);
+    await fetch(API + "/" + id,
+      {
+        method: "PUT",
+        body: JSON.stringify(editedRecipe),
+        headers: { "Content-Type": "application/json" },
+      })
+    console.log(recipeList);
+    getRecipes();
+  }
 
-                    <Modal.Footer id="footer">
-                        <Button variant="secondary" id="close-btn" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" id="submit-btn" onClick={() => {
-                            handleEditedSubmit(editedRecipe);
-                            handleClose();
-                        }}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-
-                <div className="steps">
-
-                </div>
-            </div>
-        </div >
+  const deleteRecipe = async (recipeId) => {
+    const resourceID = (recipeId);
+    const response = await fetch(
+      API + "/" + resourceID,
+      { method: "DELETE" }
     )
+    console.log("The recipe has been removed");
+    getRecipes();
+  }
+
+  return (
+    <div className="App">
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Homepage recipeList={recipeList} API={API} featuredRecipe={featuredRecipe} />}></Route>
+        <Route path="/recipes" element={<RecipesPage recipeList={recipeList} />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/new" element={<AddRecipePage addRecipeClick={addRecipeClick} />}></Route>
+        <Route path="/recipes/:recipeId" element={<RecipePage recipeList={recipeList} deleteRecipe={deleteRecipe} submitEditedRecipe={submitEditedRecipe} setId={setId} />} />
+        <Route path="/searched/:search" element={<Searched />} />
+      </Routes>
+    </div >
+  );
 }
+
